ad252320fa2635731c0bcedc2895a23905f0c722
Zachary Cipolletta <ZackCipolletta@gmail.com>
2025-08-29 14:18:30 -0700

    create text field, check if text field has value

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [textInput, setTextInput] = useState('');
+
+  const userInput = document.getElementById('userInput');
+  const userInputValue = userInput ? userInput.value : '';
+
+  const checkForUserInput = () => {
+    if (userInput) {
+      console.log(userInput.value);
+    } else {
+      console.log("userInput has no value");
+    }
+  };
+
+  const logInputText = () => {
+    console.log(userInput);
+  };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <textarea
+          id='userInput'
+          onChange={(e) => setTextInput(e.target.value)}
+        >
+
+        </textarea>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => checkForUserInput()}>
+          count is
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
