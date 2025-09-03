43570b20c6c5b0fb94d3be5a3923be0a0eaff7ec
Zachary Cipolletta <ZackCipolletta@gmail.com>
2025-09-03 13:30:50 -0700

    send input html to server and verify has lang tag

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import { JSDOM } from 'jsdom';
 
 const app = express();
 app.use(express.json());
@@ -11,12 +12,29 @@ app.listen(PORT, () => {
   console.log("Server Listening on PORT:", PORT);
 });
 
-  app.post("/test", (request, response) => {
-    const status = {
-      "Status": "Running"
-    };
-    const input = request.body.input;
-    response.send("user input: " + input);
-  });
+app.post("/test", (request, response) => {
+  const status = {
+    "Status": "Running"
+  };
+  const htmlString = request.body.html;
 
-export {app};
+  // check to verify that html string is actually present in body
+  if (!htmlString) {
+    return response.status(400).json({ error: 'No HTML provided' });
+  }
+
+  // Create a JSDOM instance from the string
+  const dom = new JSDOM(htmlString);
+  const htmlElement = dom.window.document.documentElement;
+
+  const langTag = htmlElement.hasAttribute('lang') &&
+    htmlElement.getAttribute('lang').trim() !== '';
+  
+  if (langTag) {
+
+  }
+
+  response.send("user input: " + langTag);
+});
+
+export { app };
--- server/src/validateLang.js
+++ server/src/validateLang.js
@@ -1,14 +1,14 @@
-  const validateLang = (lang) => {
-    const langAttribute = page.getAttribute('lang');
-    if (langAttribute) {
-      console.log('page includes lang attribute: ' + langAttribute);
-      if (!languageTags(langAttribute).valid()) {
-        console.log(`Invalid or unregistered BCP 47 language tag: ${langAttribute}`);
-      } else {
-        console.log('everything looks good');
-        console.log(languageTags(langAttribute).valid());
-      }
-    } else {
-      console.log(`The document's primary language is not declared.`);
-    } 
-  };
+  // const validateLang = (lang) => {
+  //   const langAttribute = page.getAttribute('lang');
+  //   if (langAttribute) {
+  //     console.log('page includes lang attribute: ' + langAttribute);
+  //     if (!languageTags(langAttribute).valid()) {
+  //       console.log(`Invalid or unregistered BCP 47 language tag: ${langAttribute}`);
+  //     } else {
+  //       console.log('everything looks good');
+  //       console.log(languageTags(langAttribute).valid());
+  //     }
+  //   } else {
+  //     console.log(`The document's primary language is not declared.`);
+  //   } 
+  // };
