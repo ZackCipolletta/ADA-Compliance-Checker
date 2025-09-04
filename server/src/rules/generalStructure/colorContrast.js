dbae3a7620dcba2803323ace17537888a9694601
Zachary Cipolletta <ZackCipolletta@gmail.com>
2025-09-04 13:05:46 -0700

    WIP return contrast ratio to client

--- server/src/rules/generalStructure/colorContrast.js
+++ server/src/rules/generalStructure/colorContrast.js
@@ -1,5 +1,34 @@
 function colorContrast(dom) {
 
+  const calculateContrastRatio = (color1, color2) => {
+    const RED = 0.2126;
+    const GREEN = 0.7152;
+    const BLUE = 0.0722;
+
+    const GAMMA = 2.4;
+
+    function luminance(r, g, b) {
+      var a = [r, g, b].map((v) => {
+        v /= 255;
+        return v <= 0.03928
+          ? v / 12.92
+          : Math.pow((v + 0.055) / 1.055, GAMMA);
+      });
+      return a[0] * RED + a[1] * GREEN + a[2] * BLUE;
+    }
+
+    function contrast(rgb1, rgb2) {
+      var lum1 = luminance(...rgb1);
+      var lum2 = luminance(...rgb2);
+
+      var brightest = Math.max(lum1, lum2);
+      var darkest = Math.min(lum1, lum2);
+      return (brightest + 0.05) / (darkest + 0.05);
+    }
+    return contrast(color1, color2);
+  };
+
+
   // select all elements in the doc
   const allElements = dom.window.document.querySelectorAll('*');
   let textContainingElements = [];
@@ -22,12 +51,19 @@ function colorContrast(dom) {
         elBackgroundColor = 'rgb(255, 255, 255)';
       }
 
+      const rgbStringToArray = (rgbString) => {
+        return rgbString.match(/\d+/g).map(Number);
+      };
+
+      const contrast = calculateContrastRatio(rgbStringToArray(elBackgroundColor), rgbStringToArray(elColor));
+
       const elData = {
         element: elTagName,
         text: el.textContent.trim(),
         color: elColor,
         backgroundColor: elBackgroundColor,
-        elementParent: elParent
+        elementParent: elParent,
+        contrastRatio: contrast
       };
       textContainingElements.push(elData);
     }
