ef85c1fe4bec2c020e327bbc3ff9e04cd5d9ff74
Zachary Cipolletta <ZackCipolletta@gmail.com>
2025-09-05 15:42:04 -0700

    Add check for img Alt text and display a message alerting about missing or empty alt text

--- server/src/rules/images.js
+++ server/src/rules/images.js
@@ -5,25 +5,26 @@ function imageAltText(dom) {
   const imgElements = dom.window.document.getElementsByTagName('img');
   const imgIssues = [];
 
-
-  imgElements.forEach(el => {
-    if (!el.alt) {
+  // Convert HTMLCollection to array for easier manipulation
+  Array.from(imgElements).forEach((img, index) => {
+    const alt = img.getAttribute('alt');
+    if (!alt && alt !== '') {
       const issue = {
         issue: `Missing 'alt' Text`,
         element: `<img>`,
-        details: `Informative images must have a descriptive 'alt' attribute. Current text: ${el.alt}`,
+        details: `The image at position ${index + 1} is missing an 'alt' attribute. Informative images must have a descriptive 'alt' attribute. Current text: ${img.alt}`,
         rule: 'IMG_ALT_MISSING',
       };
       imgIssues.push(issue);
       return {
 
       };
-    } else if (el.alt) {
+    } else if (alt === '') {
       const issue = {
-        issue: `Missing 'alt' Text`,
+        issue: `Empty 'alt' Text`,
         element: `<img>`,
-        details: `Informative images must have a descriptive 'alt' attribute. Current text: ${el.alt}`,
-        rule: 'IMG_ALT_MISSING',
+        details: `The image at position ${index + 1} is missing an EMPTY 'alt' attribute. Informative images must have a descriptive 'alt' attribute. Current text: ${img.alt}`,
+        rule: 'IMG_ALT_EMPTY',
       };
       imgIssues.push(issue);
     }
