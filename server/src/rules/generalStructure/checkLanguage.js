65bc69d1d2a86bec5aab98eaa79bf2ac29e99446
Zachary Cipolletta <ZackCipolletta@gmail.com>
2025-09-05 18:08:15 -0700

    add documentation to the checkLanguage function

--- server/src/rules/generalStructure/checkLanguage.js
+++ server/src/rules/generalStructure/checkLanguage.js
@@ -3,14 +3,21 @@ import languageTags from 'language-tags';
 function checkLanguage(dom) {
   const htmlElement = dom.window.document.documentElement;
   // check if input html has a 'lang' attribute and the attribute is not an empty string
-  if (htmlElement.hasAttribute('lang') && htmlElement.getAttribute('lang').trim() !== '') {
-    const langAttribute = htmlElement.getAttribute('lang').trim();
-
+  if (!htmlElement.hasAttribute('lang') || htmlElement.getAttribute('lang').trim() === '') {
+    return {
+      issue: `Missing 'lang' Attribute`,
+      element: `<html>`,
+      details: `The <html> element must have a valid lang attribute.`,
+      rule: 'DOC_LANG_MISSING',
+    };
+  } else {
     // check if the lang attribute is a valid language tag
+    const langAttribute = htmlElement.getAttribute('lang').trim();
     const checkValidity = languageTags(langAttribute).valid();
     // if valid tag return without issue
     if (checkValidity) {
       return null;
+      // if invalid tag return message indicating not a valid 'lang' tag
     } else {
       return {
         issue: `Invalid 'lang' Attribute`,
@@ -19,14 +26,6 @@ function checkLanguage(dom) {
         rule: 'DOC_LANG_INVALID',
       };
     }
-    // if 'lang' attribute does not exist or is an empty string
-  } else {
-    return {
-      issue: `Missing 'lang' Attribute`,
-      element: `<html>`,
-      details: `The <html> element must have a valid lang attribute.`,
-      rule: 'DOC_LANG_MISSING',
-    };
   }
 }
 
